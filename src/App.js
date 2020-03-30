0a716951433173e1a566e6e3f77ca0c86c7815b3
Pramod Gurav <60379603+pramodbgurav@users.noreply.github.com>
2020-03-30 02:09:29 -0700

    removed nav from app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './App.css';
 
-import
-{
+import {
   BrowserRouter as Router,
   Route,
   NavLink
 } from "react-router-dom";
 
-import Nav from "./components/nav";
 
 import Maincomponent from './components/main-component';
 
@@ -18,58 +16,40 @@ import User from './components/User';
 
 
 
-function App ()
-{
-  const [ posts, setPosts ] = useState([]);
-  const [ user, setUser ] = useState({ id: '', name: '' })
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState({ id: '', name: '' })
 
-  // const users = {
-  //   1: "Pramod Gurav",
-  //   2: "Sandeep Hirwale",
-  //   3: "Shivraj Sawant",
-  //   4: "Anushree Nair",
-  //   5: "Dhruv Dvivedi"
-  // };
-
-
-
-
-
-  function loadPost ()
-  {
-
+  function loadPost() {
+    console.log("in loadpost")
     return (
+      <Maincomponent allPosts={posts} ></Maincomponent>
 
-      <main>
-        <Maincomponent allPosts={ posts }></Maincomponent>
-      </main>
     )
   }
 
 
 
-  function onFormSubmitUser (userData)
-  {
+  function onFormSubmitUser(userData) {
     setUser(userData);
   }
-
 
 
   const methods = {
     onFormSubmitUser,
     user
+
   }
 
   return (
 
-    <UserContext.Provider value={ methods }>
+    <UserContext.Provider value={methods}>
       {
         user.id !== '' && user.id !== 0 ?
           <React.Fragment>
-            <Nav />
+
             <Router>
-              <div className="container"></div>
-              <Route exact path="/" render={ loadPost } />
+              <Route exact path="/" render={loadPost} />
             </Router>
           </React.Fragment>
           :
